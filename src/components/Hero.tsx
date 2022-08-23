b3b0256c650f4c272de50dabe9846d0879f0c351
Vinicius Braga <Vinicius@gcb.local>
2022-08-23 11:15:14 -0300

    Project Updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPlus } from "@fortawesome/free-solid-svg-icons";
 import placeholder from '../assets/Hero.jpg'
 
-const Hero = () => (
-   <header className='min-h-screen'>
+const Hero = ({title = 'Avangers Endgame', score = 10}) => {
+   const getBorderColor = () => {
+      if (score >= 7) {
+         return 'border-green-400';
+      } else if (score >= 4 && score < 7){ 
+         return 'border-yellow-400';
+      } else {
+         return 'border-red-400';
+      }
+   }
+   return (
+   <header className='realative min-h-screen'>
       <img className='object-cover object-center h-full w-full' 
       src={placeholder} 
       alt='File em destaque'/>
+      <article className='absolute bottom-0 mb-64 px-8'>
+         <p className='text-3xl'>Assista agora:</p>
+         <h2 className='text-6xl font-bold'>{title}</h2>
+         <p className='text-base'>Nota <span className={`inline-block py-2 px-3 border-4 bg-black bg-opacity rounded-full ${getBorderColor()} `}>{score}</span></p>
+         <button className='text-base py-2 px-8 mr-2 mt-8 rounded bg-black bg-opacity-50 trasition-all duration-300 ease-in-out hover:bg-white hover:bg-opacity-75 hover:text-black'>
+            <FontAwesomeIcon className='mr-2' icon={faPlay}/> Assistir
+         </button>
+         <button className='text-base py-2 px-8 mr-2 mt-8 rounded bg-black bg-opacity-50 trasition-all duration-300 ease-in-out hover:bg-white hover:bg-opacity-75 hover:text-black'>
+            <FontAwesomeIcon className='mr-2' icon={faPlus}/> Minha Lista
+         </button>
+      </article>              
    </header>
-);
+)
+};
 
 export default Hero;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,55 @@
+import React, {useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import logo from "../assets/imagiflix.png";
+import { faSearch, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import placehoder from "../assets/User.jpg";
+import "./NavBar.css"
+
+const NavBar = () => {
+   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+   const handleClick = () => {
+      setIsMenuOpen(!isMenuOpen);
+   }
+   return(
+   <nav className="navbar absolute top-0 left-0 grid grid-cols-2 items-center justify-between w-full p-8">
+      <div className="justify-self-start grid grid-cols-2 gap-4 items-center">
+         <h1 className="hidden">Imagiflix</h1>
+         <img src={logo} alt='Imagiflix' />
+         <ul className="grid grid-flow-col gap-4">
+            <li className="font-bold">Início</li>
+            <li><a href="#series">Séries</a></li>
+            <li><a href="#movies">Filmes</a></li>
+         </ul>
+      </div>
+
+
+      <div className="justify-self-end flex justify-items-end items-center">
+         <form className="realtive w-64">
+            <input 
+               className="w-full bg-black border border-white rounded p-1 px-3 trasition-all duration-300 ease-in-out opacity-0 hover:opacity-100 focus:opacity-100" 
+               type='text' 
+               placeholder='Títulos, gente e gêneros' />
+            <button className="absolute right-20 pr-5 mr-4 mt-1" onClick={ e => e.preventDefault()}>
+               <FontAwesomeIcon icon={faSearch} />
+            </button>
+            
+         </form>
+         <div className="relative flex ml-5">
+            <img onClick={handleClick} src={placehoder} alt='Foto do usuário' />
+            <button onClick={handleClick}>
+            <FontAwesomeIcon className="ml-2" icon={faCaretDown}/>
+            </button>
+            <ul 
+               className={`absolute mt-10 top-0 right-0 w-32 bg-black rounded p-4 trasition-all duration-300 ease-in-out  ${!isMenuOpen && 'opacity-0 invisible'}`}>
+               <li><a href="account" onClick={handleClick}>Minha conta</a></li>
+               <li><a href="logout" onClick={handleClick}>Sair</a></li>
+            </ul>
+         </div>
+      </div>
+   </nav>
+)
+};
+
+export default NavBar;
